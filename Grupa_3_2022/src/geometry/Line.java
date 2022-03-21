32c7e1acff4429391cd599848ba560c124868018
it11-2016 <it11-2016@ar5-asistent.ftn.ns>
2022-03-21 15:53:16 +0100

    Trece vezbe - klasa Line

---
+++ Grupa_3_2022/src/geometry/Line.java
@@ -0,0 +1,26 @@
+package geometry;
+
+public class Line {
+	private Point startPoint;
+	private Point endPoint;
+	private boolean selected;
+	public void setStartPoint(Point startPoint) {
+		this.startPoint=startPoint;
+	}
+	public Point getStartPoint() {
+		return this.startPoint;
+	}
+	public Point getEndPoint() {
+		return endPoint;
+	}
+	public void setEndPoint(Point endPoint) {
+		this.endPoint = endPoint;
+	}
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+}
