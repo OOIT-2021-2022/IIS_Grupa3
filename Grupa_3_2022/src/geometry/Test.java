b6172d845f3e46d0e400d99ac8e798c69206967a
it11-2016 <it11-2016@ar5-asistent.ftn.ns>
2022-03-21 16:51:05 +0100

    Trece vezbe - testiranje metoda i klasa

--- Grupa_3_2022/src/geometry/Test.java
+++ Grupa_3_2022/src/geometry/Test.java
@@ -26,14 +26,52 @@ public class Test {
 
 		Line l1 = new Line();
 		System.out.println(l1.isSelected());
-		//System.out.println(l1.getStartPoint().getX());
-		//System.out.println(l1.getEndPoint().getY());
+		// System.out.println(l1.getStartPoint().getX());
+		// System.out.println(l1.getEndPoint().getY());
 		l1.setStartPoint(p);
 		l1.setEndPoint(p1);
 		System.out.println(l1.getStartPoint().getX());
 		System.out.println(l1.getEndPoint().getY());
 		System.out.println(l1.length());
 
+		Rectangle r1 = new Rectangle();
+		Circle c1 = new Circle();
+
+		// 1. Inicijalizovati x koordinatu tacke p
+		// na vrednost y koordinate tacke p1
+		p.setX(p1.getY());
+
+		// 2. Postaviti za pocetnu tacku linije l1 tacku p, a
+		// za krajnju tacku linije l1 tacku p1
+
+		// gore je napisano
+
+		// 3. Postaviti y koordinatu krajnje tacke l1 na 23
+
+		l1.getEndPoint().setY(23);
+
+		// 4. Inicijalizovati x koordinatu pocetne tacke linije l1
+		// na vrednost y koordinate krajnje tacke linije l1
+		l1.getStartPoint().setX(l1.getEndPoint().getY());
+
+		// 5. Postaviti x koordinatu krajnje tacke l1 na vrednost
+		// duzine linije l1 umanjene za vrednost zbira x i y
+		// koordinate pocetne tacke linije l1
+		l1.getEndPoint().setX((int) l1.length() - (l1.getStartPoint().getX() + l1.getStartPoint().getY()));
+
+		// 6. Postaviti x koordinatu tacke gore levo pravougaonika
+		// r1 na vrednost 10 i y koordinatu na vrednost 15
+		r1.setUpperLeftPoint(p);
+		r1.getUpperLeftPoint().setX(10);
+		r1.getUpperLeftPoint().setY(15);
+
+		// 7. Postaviti centar kruga c1 na koordinate tacke
+		// gore levo od r1
+		c1.setCenter(r1.getUpperLeftPoint());
+
+		// 8. Postaviti x koordinatu centra kruga c1 na vrednost razlike
+		// povrsine pravougaonika r1 i y koordinate pocetne tacke linije l1
+		c1.getCenter().setX(r1.area() - l1.getStartPoint().getY());
 	}
 
 }
